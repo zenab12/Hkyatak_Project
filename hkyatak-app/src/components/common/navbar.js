bdfb52e01c94dd2c1d9690e012e93c719e2b683f
zenab12 <zenabm@std.mans.edu.eg>
2023-03-26 22:00:31 +0200

    finish home and about and contact with responsive

--- hkyatak-app/src/components/common/navbar.js
+++ hkyatak-app/src/components/common/navbar.js
@@ -1,55 +1,63 @@
-import React from "react"
+import React from "react";
 import { Link, NavLink } from "react-router-dom";
 
 const Nabvar = () => {
-return(
-<nav>
-      <h1 className="logo">
-      <NavLink
-              className="navLink"
-              to="/"
-            >
-              Hkyatak
+  return (
+    <nav>
+      <ul>
+      <input type="checkbox" />
+      <li className="toggler">
+          <img src={require("../../assets/images/menu.png")} />
+          <img src={require("../../assets/images/close.png")} />
+        </li>  
+        <li className="logo">
+          <h1 className="logo">
+            <NavLink className="navLink" to="/">
+            <img src={require("../../assets/images/viking3.png")} /> Hkyatak
             </NavLink>
-      </h1>
-  <ul className="navbar">
-  <li>
-  <NavLink
-              className={({ isActive }) => (isActive ? "active" : "navLink")}
-              to="/"
-            >
-              Home
-            </NavLink>
-  </li>
-  <li>
-  <NavLink
-              className={({ isActive }) => (isActive ? "active" : "navLink")}
-              to="/about"
-            >
-              About
-  </NavLink>
-  </li>
-  <li>
-  <NavLink
-              className={({ isActive }) => (isActive ? "active" : "navLink")}
-              to="/contact"
-            >
-              Contact
-  </NavLink>
-  </li>
+          </h1>
+        </li>
+        <li className="menu">
+          <ul className="navbar">
+            <li>
+              <NavLink
+                className={({ isActive }) => (isActive ? "active" : "navLink")}
+                to="/"
+              >
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                className={({ isActive }) => (isActive ? "active" : "navLink")}
+                to="/about"
+              >
+                About
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                className={({ isActive }) => (isActive ? "active" : "navLink")}
+                to="/contact"
+              >
+                Contact
+              </NavLink>
+            </li>
+          </ul>
+        </li>
 
-  </ul>
-  
-  <button className="light">
-  <NavLink
-              className= "navLink"
-              to="/signup"
-            >
-             Register
-  </NavLink>  
-  </button>
-</nav>
-)
-}
+        <li className="profile">
+          <button className="light">
+            <NavLink className="navLink" to="/signup">
+              Register
+            </NavLink>
+          </button>
+          {/* <span class="name">Hello, John Doe</span>
+                <img src="profile.png" alt="profil img"/> */}
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default Nabvar;
