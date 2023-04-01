02dbc9d2e48aa2c0e8c03fe629dd2925965fa894
Zien Mohammed Abdelmonaem <78083890+zenab12@users.noreply.github.com>
2023-04-01 22:07:41 +0200

    Update auth.js

--- hkyatak-app/src/services/auth.js
+++ hkyatak-app/src/services/auth.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://back-end-ashen.vercel.app/";
+const API_URL = "https://back-end-ashen.vercel.app";
 class AuthService {
   login(email, password) {
     return axios
