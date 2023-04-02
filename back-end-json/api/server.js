a1f7d8bbcfd2c3fa8cc1327577a3bccf8d743767
Zien Mohammed Abdelmonaem <78083890+zenab12@users.noreply.github.com>
2023-04-02 15:54:42 +0200

    Update server.js

--- back-end-json/api/server.js
+++ back-end-json/api/server.js
@@ -2,7 +2,7 @@
 const jsonServer = require("json-server");
 const auth = require("json-server-auth");
 const server = jsonServer.create();
-const router = jsonServer.router("../temp/db.json");
+const router = jsonServer.router("temp/db.json");
 const middlewares = jsonServer.defaults();
 
 
@@ -15,17 +15,6 @@ server.use(jsonServer.rewriter({
 server.db = router.db;
 server.use(auth);
 server.use(router);
-
-
-server.post('/posts', (req, res) => {
-  const db = router.db;
-  const { title, body,imageUrl,videoUrl,tags,userId } = req.body;
-  const id = Date.now().toString();
-  tags = tags.split(",");
-  const post = { id, title, body,imageUrl,videoUrl,tags,userId };
-  db.get('posts').push(post).write();
-  res.json(post);
-});
 
 
 server.listen(3001, () => {
