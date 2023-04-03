4f4c05e03b60cef9cfda4943f533e6515e471ced
Zien Mohammed Abdelmonaem <78083890+zenab12@users.noreply.github.com>
2023-04-03 15:30:44 +0200

    Update create_post.js

--- front-end/src/components/posts/create_post.js
+++ front-end/src/components/posts/create_post.js
@@ -19,7 +19,7 @@ const  CreatePost = () => {
         const data = { title, body,imageUrl,videoUrl,tags,userName,userId,userImg };
 
         if(data){
-        const response = await axios.post('https://hkyatak-backend.vercel.app/api/posts', data);
+        const response = await axios.post('https://back-end-vercel.vercel.app/api/posts', data);
         console.log(response.data);
         
         if(response.status == 201 || 200)
